87d8588f317124a1d282c6c5f86ccf157aab1f9b
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-08-20 17:47:28 +0300

    improved sign-in and signup request schemas for validation purposes

--- api_server/chat_with_anyone/views/auth.py
+++ api_server/chat_with_anyone/views/auth.py
@@ -1,49 +1,69 @@
+from secrets import token_urlsafe
+
 from aiohttp import web
 from aiohttp_apispec import docs, request_schema, response_schema
 from asyncpg import UniqueViolationError
-from marshmallow import Schema, fields
-from secrets import token_urlsafe
+from marshmallow import Schema, fields, validate
 
 from ..models.user import User
 
 
 class SigninRequestSchema(Schema):
-    username = fields.Str()
-    password = fields.Str()
+    email = fields.Str(
+        validate=validate.Length(max=255), required=True
+    )
+    password = fields.Str(
+        validate=validate.Length(max=255), required=True
+    )
 
 
 class SignupRequestSchema(Schema):
-    username = fields.Str()
-    email = fields.Str()
-    password = fields.Str()
+    username = fields.Str(
+        validate=validate.Length(max=40), required=True
+    )
+    email = fields.Str(
+        validate=validate.Length(max=255), required=True
+    )
+    password = fields.Str(
+        validate=validate.Length(max=255), required=True
+    )
+    first_name = fields.Str(
+        validate=validate.Length(max=30)
+    )
+    last_name = fields.Str(
+        validate=validate.Length(max=150)
+    )
 
 
 class AuthResponseSchema(Schema):
-    token = fields.Str()
+    token = fields.Str(
+        required=True, validate=validate.Length(equal=40)
+    )
 
 
-@docs(tags=['auth'], summary='User sign-in')
+@docs(tags=['Auth'], summary='Sign-in and receive a token.')
 @request_schema(SigninRequestSchema(strict=True))
-@response_schema(AuthResponseSchema(), 200)
+@response_schema(AuthResponseSchema(strict=True))
 async def sign_in(request):
     """
-    Signs user in if username exists and password is valid.
+    Signs user in if email exists and password is valid.
     :param request: content of the page to take json.
     :return: response in json representation.
     """
     data = await request.json()
     user = await User.query.where(User.email == data['email']).gino.first()
 
-    if user and user.password == data['password']:
-        await user.update(token=token_urlsafe(30)).apply()
-        return web.json_response({'token': user.token})
-    else:
-        return web.json_response(status=403)
+    if not user or user.password != data['password']:
+        return web.json_response(
+            {'message': 'Invalid credentials.'}, status=400
+        )
+
+    await user.update(token=token_urlsafe(30)).apply()
+    return web.json_response({'token': user.token})
 
 
-@docs(tags=['auth'], summary='User signup')
+@docs(tags=['Auth'], summary='Signup.')
 @request_schema(SignupRequestSchema(strict=True))
-@response_schema(AuthResponseSchema(), 201)
 async def signup(request):
     """
     Signs user up if email is unique.
@@ -51,10 +71,20 @@ async def signup(request):
     :return: response in json representation.
     """
     data = await request.json()
+
     try:
-        await User.create(username=data['username'], email=data['email'], password=data['password'],
-                          token=token_urlsafe(30))
+        await User.create(
+            username=data['username'],
+            email=data['email'],
+            password=data['password'],
+            first_name=data.get('first_name'),
+            last_name=data.get('last_name'),
+            token=token_urlsafe(30)
+        )
     except UniqueViolationError as e:
-        return web.json_response({'message': e.as_dict()['detail']})
+        return web.json_response(
+            {'message': e.as_dict()['detail']},
+            status=400
+        )
 
     return web.json_response(status=201)
