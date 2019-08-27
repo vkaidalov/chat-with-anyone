00d71a77c6d50becf1a776dc2d178629afcae086
Anna Basova <annabassova152@gmail.com>
2019-08-27 18:13:02 +0300

    Create initial tests for chats

---
+++ api_server/chat_with_anyone/tests/test_chats.py
@@ -0,0 +1,23 @@
+import pytest
+from aiohttp import web
+
+from ..routes import setup_routes
+
+
+@pytest.fixture
+def cli(loop, aiohttp_client):
+    app = web.Application()
+    setup_routes(app)
+    return loop.run_until_complete(aiohttp_client(app))
+
+
+async def test_get_list_chats(cli):
+    resp = await cli.get('/api/chats/')
+    assert resp.status == 200
+    assert await resp.json() == []
+
+
+async def test_post_chats(cli):
+    resp = await cli.post('/api/chats/', json={'key': 'value'})
+    assert resp.status == 201
+    assert await resp.json() == {}
