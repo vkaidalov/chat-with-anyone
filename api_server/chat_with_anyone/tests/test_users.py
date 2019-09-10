3c3a208ac64cd20024564d23168bc448c5f6adcd
dziubakseniia <dziubakseniia@gmail.com>
2019-09-10 23:16:31 +0300

    Add get users test

--- api_server/chat_with_anyone/tests/test_users.py
+++ api_server/chat_with_anyone/tests/test_users.py
@@ -2,27 +2,22 @@ from aiohttp.test_utils import AioHTTPTestCase, unittest_run_loop, make_mocked_r
 from aiohttp import web
 
 from ..routes import setup_routes
-from ..models.group_room import GroupRoom
+from ..models.user import User
 
 
-class ChatTestCase(AioHTTPTestCase):
+class UserTestCase(AioHTTPTestCase):
     async def get_application(self):
         app = web.Application()
         setup_routes(app)
         return app
 
     @unittest_run_loop
-    async def test_get_list_chats(self):
+    async def test_get_users(self):
         req = make_mocked_request('GET', '/api/users',
                                   headers={'Authorization': 'token'})
         assert req.headers.get('Authorization') == 'token'
-        chats = str([GroupRoom()])
-        resp = web.Response(body=bytes(chats, 'utf-8'))
+        users = str([User(id=1, username='username')])
+        resp = web.Response(body=bytes(users, 'utf-8'))
+        assert resp.body.decode() == users
         assert resp.status == 200
-        assert resp.body.decode() == chats
 
-    async def test_post_chats(cli):
-        pass
-        # resp = await cli.post('/api/chats/', json={'key': 'value'})
-        # assert resp.status == 201
-        # assert await resp.json() == {}
