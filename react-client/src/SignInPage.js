66136a6b3ed64cb788201f1bd0be249441312f64
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-09-20 19:33:29 +0300

    Fill SignInPage with tags and styles

--- react-client/src/SignInPage.js
+++ react-client/src/SignInPage.js
@@ -1,7 +1,37 @@
 import React from "react";
+import {Link} from "react-router-dom";
+
+import "./SignInSignUpPage.css";
 
 function SignInPage() {
-    return <h2>Sign In</h2>;
+    return (
+        <div className="wrapper">
+            <div className="user-icon">
+                <img
+                    src="https://github.com/hesoyam11/chat-with-anyone/raw/feature-front-end-register/react-client/src/assets/img/user-icon.png"
+                    alt=""/>
+            </div>
+
+            <form className="form">
+                <div className="form__item input-field inline">
+                    <input className="form__item_input validate"
+                           id="e-mail" placeholder="E-mail" type="email" required/>
+                </div>
+
+                <div className="form__item input-field inline">
+                    <input className="form__item_input validate"
+                           id="password" placeholder="Password" type="password" required/>
+                </div>
+
+                <div>
+                    <button type="submit" className="form__item btn waves-effect waves-light">Sign In</button>
+                    <div className="additional_links">
+                        <Link className="register" to="/signup">Sign Up</Link>
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default SignInPage;
