300bc9ced0ab09f13e59a0bcbf0ad33fdb6e41aa
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-09-21 11:55:57 +0300

    Sign in and redirect to home page on success

--- react-client/src/SignInPage.js
+++ react-client/src/SignInPage.js
@@ -1,36 +1,72 @@
 import React from "react";
 import {Link} from "react-router-dom";
 
+import axios from "./axiosBaseInstance";
+import handleInputChange from "./utils";
+
 import UserIcon from "./user-icon.png";
 import "./SignInSignUpPage.css";
 
-function SignInPage() {
-    return (
-        <div className="wrapper">
-            <div className="user-icon">
-                <img src={UserIcon} alt=""/>
-            </div>
+class SignInPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: ''
+        };
+        this.handleInputChange = handleInputChange.bind(this);
+        this.handleSignInFormSubmit = this.handleSignInFormSubmit.bind(this);
+    }
 
-            <form className="form">
-                <div className="form__item input-field inline">
-                    <input className="form__item_input validate"
-                           id="e-mail" placeholder="E-mail" type="email" required/>
+    handleSignInFormSubmit(event) {
+        event.preventDefault();
+        const data = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        axios.post("api/sign-in", data)
+            .then(response => {
+                localStorage.setItem("token", response.data["token"]);
+                localStorage.setItem("userId", response.data["user_id"]);
+                this.props.history.push("/home");
+            })
+            .catch(error => {
+                alert(error.response.data["message"]);
+            });
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="user-icon">
+                    <img src={UserIcon} alt=""/>
                 </div>
 
-                <div className="form__item input-field inline">
-                    <input className="form__item_input validate"
-                           id="password" placeholder="Password" type="password" required/>
-                </div>
-
-                <div>
-                    <button type="submit" className="form__item btn waves-effect waves-light">Sign In</button>
-                    <div className="additional_links">
-                        <Link className="register" to="/signup">Sign Up</Link>
+                <form className="form" onSubmit={this.handleSignInFormSubmit}>
+                    <div className="form__item input-field inline">
+                        <input className="form__item_input validate"
+                               name="email" onChange={this.handleInputChange}
+                               placeholder="E-mail" type="email" required/>
                     </div>
-                </div>
-            </form>
-        </div>
-    );
+
+                    <div className="form__item input-field inline">
+                        <input className="form__item_input validate"
+                               name="password" onChange={this.handleInputChange}
+                               placeholder="Password" type="password" required/>
+                    </div>
+
+                    <div>
+                        <button type="submit" className="form__item btn waves-effect waves-light">
+                            Sign In
+                        </button>
+                        <div className="additional_links">
+                            <Link className="register" to="/signup">Sign Up</Link>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default SignInPage;
